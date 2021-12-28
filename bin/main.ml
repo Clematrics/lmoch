5b4f385253a37ed7f0811bd24d8f79f1a8637c02
Clematrics <clematrics.studio@gmail.com>
2021-12-28 19:19:07 +0100

    Integration of Alt-Ergo-Zero

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let rec fact = function
+  | n when n <= 0 ->
+      raise
+        (Invalid_argument "Cannot compute the factorial of a negative number")
+  | 1 -> 1
+  | n -> fact (n - 1) * n
+
+let () =
+  print_int (fact 10);
+  print_newline ();
+  print_endline "Hello, World!"
